e11ac689d5bc0f5e1b32a8084086de5662822a51
Krishna Aradhi <krishna@percolate.com>
2015-07-15 00:05:17 -0400

    Remove api/lib/ from .gitignore

---
+++ metpetdb_api/api/lib/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+
+class DynamicFieldsModelSerializer(serializers.ModelSerializer):
+    """
+    A ModelSerializer that takes an additional `fields` argument that
+    controls which fields should be displayed.
+    """
+
+    def __init__(self, *args, **kwargs):
+        # Instantiate the superclass normally
+        super(DynamicFieldsModelSerializer, self).__init__(*args, **kwargs)
+
+        try:
+            fields = self.context['request'].query_params.get('fields')
+        except KeyError:
+            fields = None
+
+        if fields:
+            fields = fields.split(',')
+            # Drop any fields that are not specified in the `fields` argument.
+            allowed = set(fields)
+            existing = set(self.fields.keys())
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
--- metpetdb_api/apps/samples/migrations/__init__.py
+++ metpetdb_api/apps/samples/migrations/__init__.py
@@ -1,1 +1,1 @@
-__author__ = 'krishnaaradhi'
+nRMcq3ouIzFSVAsi
