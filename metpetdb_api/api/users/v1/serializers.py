da71d73ea1506ee0f56ec41d9228fe783cfc02ad
Krishna Aradhi <krishna@percolate.com>
2015-07-20 22:12:11 -0400

    Expose only a few user fields for now

--- metpetdb_api/api/users/v1/serializers.py
+++ metpetdb_api/api/users/v1/serializers.py
@@ -6,6 +6,4 @@ from api.lib.serializers import DynamicFieldsModelSerializer
 class UserSerializer(DynamicFieldsModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ('id', 'email', 'name', 'address', 'city', 'province',
-                  'country', 'postal_code', 'is_active', 'is_contributor',
-                  'professional_url', 'is_superuser')
+        fields = ('id', 'email', 'name',)
