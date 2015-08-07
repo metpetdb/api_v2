708ad4313255a4295b55273451195466dd68158c
Krishna Aradhi <krishna@percolate.com>
2015-08-06 22:07:38 -0400

    Some changes to sample-related models

--- metpetdb_api/apps/samples/migrations/0001_initial.py
+++ metpetdb_api/apps/samples/migrations/0001_initial.py
@@ -2,22 +2,25 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import concurrency.fields
+import django.contrib.postgres.fields
+from django.conf import settings
 import django.contrib.gis.db.models.fields
 import uuid
-import django.contrib.postgres.fields
+import concurrency.fields
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('chemical_analyses', '0001_initial'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Collector',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, primary_key=True, editable=False)),
+                ('id', models.UUIDField(editable=False, default=uuid.uuid4, primary_key=True, serialize=False)),
                 ('name', models.CharField(unique=True, max_length=50)),
             ],
             options={
@@ -27,7 +30,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Country',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, primary_key=True, editable=False)),
+                ('id', models.UUIDField(editable=False, default=uuid.uuid4, primary_key=True, serialize=False)),
                 ('name', models.CharField(unique=True, max_length=100)),
             ],
             options={
@@ -37,7 +40,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Grid',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, primary_key=True, editable=False)),
+                ('id', models.UUIDField(editable=False, default=uuid.uuid4, primary_key=True, serialize=False)),
                 ('version', concurrency.fields.AutoIncVersionField(help_text='record revision number', default=0)),
                 ('width', models.SmallIntegerField()),
                 ('height', models.SmallIntegerField()),
@@ -50,7 +53,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='MetamorphicGrade',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, primary_key=True, editable=False)),
+                ('id', models.UUIDField(editable=False, default=uuid.uuid4, primary_key=True, serialize=False)),
                 ('name', models.CharField(unique=True, max_length=100)),
             ],
             options={
@@ -60,11 +63,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='MetamorphicRegion',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, primary_key=True, editable=False)),
+                ('id', models.UUIDField(editable=False, default=uuid.uuid4, primary_key=True, serialize=False)),
                 ('name', models.CharField(unique=True, max_length=100)),
-                ('shape', django.contrib.gis.db.models.fields.GeometryField(srid=4326, null=True, blank=True)),
-                ('description', models.TextField(null=True, blank=True)),
-                ('label_location', django.contrib.gis.db.models.fields.GeometryField(srid=4326, null=True, blank=True)),
+                ('shape', django.contrib.gis.db.models.fields.GeometryField(blank=True, srid=4326, null=True)),
+                ('description', models.TextField(blank=True, null=True)),
+                ('label_location', django.contrib.gis.db.models.fields.GeometryField(blank=True, srid=4326, null=True)),
             ],
             options={
                 'db_table': 'metamorphic_regions',
@@ -73,17 +76,41 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Mineral',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, primary_key=True, editable=False)),
+                ('id', models.UUIDField(editable=False, default=uuid.uuid4, primary_key=True, serialize=False)),
                 ('name', models.CharField(unique=True, max_length=100)),
+                ('real_mineral', models.ForeignKey(blank=True, null=True, to='samples.Mineral')),
             ],
             options={
                 'db_table': 'minerals',
             },
         ),
         migrations.CreateModel(
+            name='MineralRelationship',
+            fields=[
+                ('id', models.UUIDField(editable=False, default=uuid.uuid4, primary_key=True, serialize=False)),
+                ('child_mineral', models.ForeignKey(to='samples.Mineral', related_name='child')),
+                ('parent_mineral', models.ForeignKey(to='samples.Mineral', related_name='parent')),
+            ],
+            options={
+                'db_table': 'mineral_relationships',
+            },
+        ),
+        migrations.CreateModel(
+            name='MineralType',
+            fields=[
+                ('id', models.UUIDField(editable=False, default=uuid.uuid4, primary_key=True, serialize=False)),
+                ('name', models.CharField(max_length=50)),
+                ('elements', models.ManyToManyField(to='chemical_analyses.Element', related_name='mineral_types')),
+                ('oxides', models.ManyToManyField(to='chemical_analyses.Oxide', related_name='mineral_types')),
+            ],
+            options={
+                'db_table': 'mineral_types',
+            },
+        ),
+        migrations.CreateModel(
             name='Reference',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, primary_key=True, editable=False)),
+                ('id', models.UUIDField(editable=False, default=uuid.uuid4, primary_key=True, serialize=False)),
                 ('name', models.CharField(unique=True, max_length=100)),
             ],
             options={
@@ -93,7 +120,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Region',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, primary_key=True, editable=False)),
+                ('id', models.UUIDField(editable=False, default=uuid.uuid4, primary_key=True, serialize=False)),
                 ('name', models.CharField(unique=True, max_length=100)),
             ],
             options={
@@ -103,7 +130,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='RockType',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, primary_key=True, editable=False)),
+                ('id', models.UUIDField(editable=False, default=uuid.uuid4, primary_key=True, serialize=False)),
                 ('name', models.CharField(unique=True, max_length=100)),
             ],
             options={
@@ -113,22 +140,25 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Sample',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, primary_key=True, editable=False)),
+                ('id', models.UUIDField(editable=False, default=uuid.uuid4, primary_key=True, serialize=False)),
                 ('version', concurrency.fields.AutoIncVersionField(help_text='record revision number', default=0)),
                 ('public_data', models.BooleanField(default=False)),
                 ('number', models.CharField(max_length=35)),
-                ('aliases', django.contrib.postgres.fields.ArrayField(size=None, null=True, base_field=models.CharField(max_length=35, blank=True), blank=True)),
-                ('collection_date', models.DateTimeField(null=True, blank=True)),
-                ('description', models.TextField(null=True, blank=True)),
-                ('location_name', models.CharField(null=True, max_length=50, blank=True)),
+                ('aliases', django.contrib.postgres.fields.ArrayField(size=None, blank=True, base_field=models.CharField(blank=True, max_length=35), null=True)),
+                ('collection_date', models.DateTimeField(blank=True, null=True)),
+                ('description', models.TextField(blank=True, null=True)),
+                ('location_name', models.CharField(blank=True, max_length=50, null=True)),
                 ('location_coords', django.contrib.gis.db.models.fields.PointField(srid=4326)),
-                ('location_error', models.FloatField(null=True, blank=True)),
-                ('date_precision', models.SmallIntegerField(null=True, blank=True)),
-                ('country', models.CharField(null=True, max_length=100, blank=True)),
-                ('regions', django.contrib.postgres.fields.ArrayField(size=None, null=True, base_field=models.CharField(max_length=100, blank=True), blank=True)),
-                ('references', django.contrib.postgres.fields.ArrayField(size=None, null=True, base_field=models.CharField(max_length=100, blank=True), blank=True)),
-                ('collector_name', models.CharField(null=True, max_length=50, blank=True)),
-                ('sesar_number', models.CharField(null=True, max_length=9, blank=True)),
+                ('location_error', models.FloatField(blank=True, null=True)),
+                ('date_precision', models.SmallIntegerField(blank=True, null=True)),
+                ('country', models.CharField(blank=True, max_length=100, null=True)),
+                ('regions', django.contrib.postgres.fields.ArrayField(size=None, blank=True, base_field=models.CharField(blank=True, max_length=100), null=True)),
+                ('references', django.contrib.postgres.fields.ArrayField(size=None, blank=True, base_field=models.CharField(blank=True, max_length=100), null=True)),
+                ('collector_name', models.CharField(blank=True, max_length=50, null=True)),
+                ('sesar_number', models.CharField(blank=True, max_length=9, null=True)),
+                ('collector_id', models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, null=True, db_column='collector_id', related_name='+')),
+                ('metamorphic_grades', models.ManyToManyField(to='samples.MetamorphicGrade')),
+                ('metamorphic_regions', models.ManyToManyField(to='samples.MetamorphicRegion')),
             ],
             options={
                 'db_table': 'samples',
@@ -137,8 +167,10 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='SampleMineral',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, primary_key=True, editable=False)),
-                ('amount', models.CharField(null=True, max_length=30, blank=True)),
+                ('id', models.UUIDField(editable=False, default=uuid.uuid4, primary_key=True, serialize=False)),
+                ('amount', models.CharField(blank=True, max_length=30, null=True)),
+                ('mineral', models.ForeignKey(to='samples.Mineral')),
+                ('sample', models.ForeignKey(to='samples.Sample')),
             ],
             options={
                 'db_table': 'sample_minerals',
@@ -147,10 +179,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Subsample',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, primary_key=True, editable=False)),
+                ('id', models.UUIDField(editable=False, default=uuid.uuid4, primary_key=True, serialize=False)),
                 ('name', models.CharField(max_length=100)),
                 ('version', concurrency.fields.AutoIncVersionField(help_text='record revision number', default=0)),
                 ('public_data', models.BooleanField(default=False)),
+                ('owner', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
                 ('sample', models.ForeignKey(to='samples.Sample')),
             ],
             options={
@@ -160,7 +193,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='SubsampleType',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, primary_key=True, editable=False)),
+                ('id', models.UUIDField(editable=False, default=uuid.uuid4, primary_key=True, serialize=False)),
                 ('name', models.CharField(unique=True, max_length=100)),
             ],
             options={
@@ -171,5 +204,29 @@ class Migration(migrations.Migration):
             model_name='subsample',
             name='subsample_type',
             field=models.ForeignKey(to='samples.SubsampleType'),
+        ),
+        migrations.AddField(
+            model_name='sample',
+            name='minerals',
+            field=models.ManyToManyField(through='samples.SampleMineral', to='samples.Mineral', related_name='samples'),
+        ),
+        migrations.AddField(
+            model_name='sample',
+            name='owner',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='samples'),
+        ),
+        migrations.AddField(
+            model_name='sample',
+            name='rock_type',
+            field=models.ForeignKey(to='samples.RockType'),
+        ),
+        migrations.AddField(
+            model_name='grid',
+            name='subsample',
+            field=models.ForeignKey(to='samples.Subsample'),
+        ),
+        migrations.AlterUniqueTogether(
+            name='mineralrelationship',
+            unique_together=set([('parent_mineral', 'child_mineral')]),
         ),
     ]
