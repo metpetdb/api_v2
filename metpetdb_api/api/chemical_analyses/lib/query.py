6582dd08db69991a4062e3ede38f847ca088adcd
Krishna Aradhi <krishna@percolate.com>
2015-08-17 10:32:14 -0400

    Elements/oxides_and search for chemical analyses

--- metpetdb_api/api/chemical_analyses/lib/query.py
+++ metpetdb_api/api/chemical_analyses/lib/query.py
@@ -3,10 +3,41 @@ def chemical_analysis_query(params, qs):
         qs = qs.filter(minerals__name__in=params['minerals'].split(','))
 
     if params.get('elements'):
-        qs = qs.filter(elements__name__in=params['elements'].split(','))
+        elements = params['elements'].split(',')
+        if params.get('elements_and'):
+            for element in elements:
+                qs = qs.extra(where=["""
+                        EXISTS (
+                            SELECT 0
+                            FROM chemical_analysis_elements cae
+                            INNER JOIN elements e
+                            ON cae.element_id = e.id
+                            WHERE chemical_analyses.id = cae.chemical_analysis_id
+                            AND e.name = %s
+                        )
+                     """], params=[element])
+        else:
+            qs = qs.filter(elements__name__in=params['elements'].split(','))
 
     if params.get('oxides'):
-        qs = qs.filter(oxides__species__in=params['oxides'].split(','))
+        oxides = params['oxides'].split(',')
+        if params.get('oxides_and'):
+            for oxide in oxides:
+                qs = qs.extra(where=["""
+                        EXISTS (
+                            SELECT 0
+                            FROM chemical_analysis_oxides cao
+                            INNER JOIN oxides o
+                            ON cao.oxide_id = o.id
+                            WHERE chemical_analyses.id = cao.chemical_analysis_id
+                            AND o.species = %s
+                        )
+                     """], params=[oxide])
+        else:
+            qs = qs.filter(oxides__species__in=params['oxides'].split(','))
+
+    if params.get('subsample_ids'):
+        qs = qs.filter(subsample_id__in=params.get('subsample_ids').split(','))
 
     if params.get('public_data'):
         if params['public_data'] == 'True':
