c41e28720712c3c97deea60e7f17bf0319152be5
Krishna Aradhi <krishna@percolate.com>
2015-08-17 23:10:09 -0400

    Return subsample_ids and chemical_analyses_ids in Sample responses

--- metpetdb_api/api/samples/v1/serializers.py
+++ metpetdb_api/api/samples/v1/serializers.py
@@ -1,18 +1,27 @@
 from rest_framework import serializers
+
 from api.lib.serializers import DynamicFieldsModelSerializer
 from api.users.v1.serializers import UserSerializer
 
+from apps.chemical_analyses.models import ChemicalAnalysis
 from apps.samples.models import (
     MetamorphicGrade,
     MetamorphicRegion,
     RockType,
     Sample,
-    Mineral, SampleMineral, Region, Reference, Collector, Subsample)
+    Mineral,
+    SampleMineral,
+    Region,
+    Reference,
+    Collector,
+    Subsample,
+)
 
 
 class RockTypeSerializer(DynamicFieldsModelSerializer):
     class Meta:
         model = RockType
+
 
 class MineralSerializer(DynamicFieldsModelSerializer):
     class Meta:
@@ -35,9 +44,23 @@ class SampleSerializer(DynamicFieldsModelSerializer):
     minerals = SampleMineralSerializer(source='samplemineral_set',
                                        many=True)
     owner = UserSerializer()
+
+    # TODO: figure out if there is a better, more efficient way to do this
+    subsample_ids = serializers.SerializerMethodField()
+    chemical_analyses_ids = serializers.SerializerMethodField()
+
     class Meta:
         model = Sample
         depth = 1
+
+    def get_subsample_ids(self, obj):
+        return Subsample.objects.filter(sample_id=obj.pk).values_list('id',
+                                                                      flat=True)
+
+    def get_chemical_analyses_ids(self, obj):
+        subsample_ids = obj.subsamples.values_list('id', flat=True)
+        return ChemicalAnalysis.objects.filter(
+            subsample_id__in=subsample_ids).values_list('id', flat=True)
 
 
 class SubsampleSerializer(DynamicFieldsModelSerializer):
