32f8b154b4cc1f87b8dca4717c8b988ef6b13e3b
Krishna Aradhi <krishna@percolate.com>
2015-09-08 22:04:31 -0400

    Recreate migrations

--- metpetdb_api/apps/chemical_analyses/migrations/0002_auto_20150907_2005.py
+++ metpetdb_api/apps/chemical_analyses/migrations/0002_auto_20150907_2005.py
@@ -8,31 +8,31 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('chemical_analyses', '0001_initial'),
         ('samples', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('chemical_analyses', '0001_initial'),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='chemicalanalyses',
+            model_name='chemicalanalysis',
             name='mineral',
-            field=models.ForeignKey(blank=True, null=True, to='samples.Mineral'),
+            field=models.ForeignKey(null=True, to='samples.Mineral', blank=True),
         ),
         migrations.AddField(
-            model_name='chemicalanalyses',
+            model_name='chemicalanalysis',
             name='owner',
             field=models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='chemical_analyses'),
         ),
         migrations.AddField(
-            model_name='chemicalanalyses',
+            model_name='chemicalanalysis',
             name='oxides',
             field=models.ManyToManyField(through='chemical_analyses.ChemicalAnalysisOxide', to='chemical_analyses.Oxide'),
         ),
         migrations.AddField(
-            model_name='chemicalanalyses',
+            model_name='chemicalanalysis',
             name='subsample',
-            field=models.ForeignKey(to='samples.Subsample'),
+            field=models.ForeignKey(to='samples.Subsample', related_name='chemical_analyses'),
         ),
         migrations.AlterUniqueTogether(
             name='chemicalanalysisoxide',
