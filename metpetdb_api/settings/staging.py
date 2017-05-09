e5d22aa61606c9c09e3d68f69851406ffa1e04cb
Lucien Christie-Dervaux <lucienchristiedervaux@gmail.com>
2017-05-09 00:17:18 -0400

    misspelled djoser

--- metpetdb_api/settings/staging.py
+++ metpetdb_api/settings/staging.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = (
     'rest_framework',
     'rest_framework.authtoken',
     'django_extensions',
-    'vendor.djsoer.djoser',
+    'vendor.djoser.djoser',
     'legacy',
     'api',
     'apps',
