718f3d555a7cc13d00424ee7ac69ddaf6b0276fb
metpetDB <irelaa2@rpi.edu>
2019-06-13 14:26:22 -0400

    quick n dirty handling of single image file upload

--- metpetdb_api/api/images/v1/views.py
+++ metpetdb_api/api/images/v1/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from api.images.v1.serializers import ImageContainerSerializer, ImageSerializer
 from apps.images.models import ImageContainer, Image
@@ -6,14 +8,43 @@ from api.lib.permissions import IsOwnerOrReadOnly, IsSuperuserOrReadOnly
 
 
 class ImageContainerViewSet(viewsets.ModelViewSet):
-    queryset = ImageContainer.objects.all()
-    serializer_class = ImageContainerSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly,)
+  queryset = ImageContainer.objects.all()
+  serializer_class = ImageContainerSerializer
+  permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                        IsOwnerOrReadOnly,)
 
 
 class ImageViewSet(viewsets.ModelViewSet):
-    queryset = Image.objects.all()
-    serializer_class = ImageSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+  queryset = Image.objects.all()
+  serializer_class = ImageSerializer
+  permission_classes = (permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly)
+
+  def get_serializer(self, *args, **kwargs):
+    # not sure what this is doing yet
+    # if self.request.method == 'PUT':
+      # kwargs['partial'] = True
+    return super().get_serializer(*args, **kwargs)
+
+  def perform_create(self, serializer):
+    return serializer.save()
+
+  def create(self, request, *args, **kwargs):
+    img_file = request.FILES.get('image')
+    if not img_file:
+      print("no image file")
+      return Response(status=404)
+
+    img_data = request.data
+    img_data['image'] = img_file
+
+
+    serializer = self.get_serializer(data=img_data)
+    serializer.is_valid(raise_exception=True)
+
+    instance = self.perform_create(serializer)
+
+    headers = self.get_success_headers(serializer.data)
+    return Response(serializer.data,
+                    status=status.HTTP_201_CREATED,
+                    headers=headers)
